d48d3471ef20d79cb05f722c397e7f518d7ca365
Thomas <code@impierium.de>
2011-01-31 23:31:22 +0100

    Purchase date input is now a date picker.

--- src/net/sourcewalker/garanbot/EditItemActivity.java
+++ src/net/sourcewalker/garanbot/EditItemActivity.java
@@ -1,10 +1,16 @@
 package net.sourcewalker.garanbot;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 /**
  * This activity should enable the user to create and edit items in the local
@@ -18,30 +24,140 @@ public class EditItemActivity extends Activity {
 
     private Button applyEditButton;
     private Button cancelEditButton;
+    private Button mPickDate;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
 
+    static final int DATE_DIALOG_ID = 0;
+
+    /**
+     * OnClickListener for the apply edit button. It retrieves the data from the
+     * editable fields and stores them to the database.
+     */
+    private final OnClickListener applyEditListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+            String name = ((EditText) findViewById(R.id.item_name)).getText()
+                    .toString();
+            String manufacturer = ((EditText) findViewById(R.id.item_manufacturer))
+                    .getText().toString();
+            String itemtype = ((EditText) findViewById(R.id.item_itemtype))
+                    .getText().toString();
+            String location = ((EditText) findViewById(R.id.item_location))
+                    .getText().toString();
+            String notes = ((EditText) findViewById(R.id.item_notes)).getText()
+                    .toString();
+            String purchaseDate = ((EditText) findViewById(R.id.item_purchasedate))
+                    .getText().toString();
+            String vendor = ((EditText) findViewById(R.id.item_vendor))
+                    .getText().toString();
+
+            saveItem(name, manufacturer, itemtype, location, notes,
+                    purchaseDate, vendor);
+        }
+
+        private void saveItem(String name, String manufacturer,
+                String itemtype, String location, String notes,
+                String purchaseDate, String vendor) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    /**
+     * OnClickListener to cancel the item edit/creation.
+     */
+    private final OnClickListener cancelEditListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    /**
+     * OnClickListener to launch a date picker if the purchase date button is
+     * clicked.
+     */
+    private final OnClickListener datePickListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            showDialog(DATE_DIALOG_ID);
+        }
+
+    };
+
+    /**
+     * Callback method, which is executed when the user sets the date in the
+     * date picker dialog. The callback updates the text display of the date
+     * picker button to the new date.
+     */
+    private final DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            updateDisplay();
+        }
+    };
+
+    /**
+     * 
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_item);
+
+        // capture our View elements
         applyEditButton = (Button) findViewById(R.id.apply_button);
-        applyEditButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-
-            }
-        });
-
         cancelEditButton = (Button) findViewById(R.id.cancel_button);
-        cancelEditButton.setOnClickListener(new OnClickListener() {
+        mPickDate = (Button) findViewById(R.id.item_purchasedate);
 
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
+        // add a click listeners
+        applyEditButton.setOnClickListener(applyEditListener);
+        cancelEditButton.setOnClickListener(cancelEditListener);
+        mPickDate.setOnClickListener(datePickListener);
 
-            }
-        });
+        // get the current date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // display the current date
+        updateDisplay();
+
+    }
+
+    /**
+     * Updates the date on the date picker button.
+     */
+    private void updateDisplay() {
+        mPickDate.setText(new StringBuilder()
+                // Month is 0 based so add 1
+                .append(mMonth + 1).append("-").append(mDay).append("-")
+                .append(mYear).append(" "));
+    }
+
+    /**
+     * Displays the date picker dialog.
+     */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case DATE_DIALOG_ID:
+            return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
+                    mDay);
+        }
+        return null;
     }
 
 }
