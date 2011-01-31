13b0a8983fa525fe48822d0f8d2c759a4d4e35fa
Thomas <code@impierium.de>
2011-01-31 19:40:54 +0100

    Implementation of item edit/add dialog.

--- src/net/sourcewalker/garanbot/EditItemActivity.java
+++ src/net/sourcewalker/garanbot/EditItemActivity.java
@@ -1,6 +1,10 @@
 package net.sourcewalker.garanbot;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * This activity should enable the user to create and edit items in the local
@@ -11,5 +15,33 @@ import android.app.Activity;
  * @author Xperimental
  */
 public class EditItemActivity extends Activity {
+
+    private Button applyEditButton;
+    private Button cancelEditButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_item);
+        applyEditButton = (Button) findViewById(R.id.apply_button);
+        applyEditButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
+        cancelEditButton = (Button) findViewById(R.id.cancel_button);
+        cancelEditButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+    }
 
 }
