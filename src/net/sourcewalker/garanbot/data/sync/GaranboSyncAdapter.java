ecdd4d1109923ed7d3e7c548ea74c0620cff8cac
Robert <xperimental@solidproject.de>
2011-03-06 14:16:14 +0100

    [GBOT-11] Implemented part of two-way sync.

--- src/net/sourcewalker/garanbot/data/sync/GaranboSyncAdapter.java
+++ src/net/sourcewalker/garanbot/data/sync/GaranboSyncAdapter.java
@@ -1,7 +1,9 @@
 package net.sourcewalker.garanbot.data.sync;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.sourcewalker.garanbot.api.AuthenticationException;
 import net.sourcewalker.garanbot.api.ClientException;
@@ -18,6 +20,7 @@ import android.content.ContentUris;
 import android.content.Context;
 import android.content.SyncResult;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
@@ -92,6 +95,9 @@ public class GaranboSyncAdapter extends AbstractThreadedSyncAdapter {
             } else {
                 // More complicated sync...
                 Log.d(TAG, "Both have content: have to sync...");
+                Map<Integer, List<Integer>> localIdMap = getLocalIdMap(provider);
+                synchronizeItems(client, provider, idList, localIdMap,
+                        syncResult);
             }
         } catch (AuthenticationException e) {
             Log.e(TAG, "Authentication error.");
@@ -104,6 +110,141 @@ public class GaranboSyncAdapter extends AbstractThreadedSyncAdapter {
             syncResult.stats.numIoExceptions++;
         }
         Log.d(TAG, "Sync ended.");
+    }
+
+    /**
+     * Do a bi-directional sync with the server.
+     * 
+     * @param client
+     *            Client object for communicating with the server.
+     * @param provider
+     *            Content provider to use.
+     * @param idList
+     *            List of items on server.
+     * @param localIdMap
+     *            For mapping server IDs to local IDs.
+     * @param syncResult
+     *            Used for synchronization statistics.
+     * @throws ClientException
+     *             If communication with the server fails.
+     * @throws RemoteException
+     *             If communication with the database fails.
+     */
+    private void synchronizeItems(GaranboClient client,
+            ContentProviderClient provider, List<Integer> idList,
+            Map<Integer, List<Integer>> localIdMap, SyncResult syncResult)
+            throws RemoteException, ClientException {
+        if (localIdMap.containsKey(Item.UNKNOWN_ID)) {
+            // First upload all new items...
+            Log.d(TAG, "Sync: local-only items...");
+            List<Integer> newItems = localIdMap.remove(Item.UNKNOWN_ID);
+            for (Integer id : newItems) {
+                Item localItem = getLocalItem(provider, id);
+                int serverId = client.item().create(localItem);
+                localItem.setServerId(serverId);
+                provider.update(ContentUris.withAppendedId(
+                        GaranboItemsProvider.CONTENT_URI_ITEMS, id), localItem
+                        .toContentValues(), null, null);
+                syncResult.stats.numInserts++;
+                Log.d(TAG, "Uploaded local-only: " + id + " --> " + serverId);
+            }
+        }
+        for (int serverId : idList) {
+            Log.d(TAG, "Sync server item " + serverId);
+            if (localIdMap.containsKey(serverId)) {
+                // Item exists locally...
+                Log.d(TAG, "  Local available.");
+                List<Integer> localIdList = localIdMap.remove(serverId);
+                if (localIdList.size() != 1) {
+                    // Something is wrong...
+                    Log.e(TAG, "More than one local item for server ID: "
+                            + serverId + " , " + localIdList);
+                }
+                int localId = localIdList.get(0);
+                Item localItem = getLocalItem(provider, localId);
+                Item serverItem = client.item().get(serverId);
+                // TODO update old item
+            } else {
+                // Item is new...
+                Log.d(TAG, "  No local item. Creating...");
+                Item serverItem = client.item().get(serverId);
+                Uri itemUri = provider.insert(
+                        GaranboItemsProvider.CONTENT_URI_ITEMS,
+                        serverItem.toContentValues());
+                syncResult.stats.numInserts++;
+                Log.d(TAG, "  New local item: " + itemUri);
+            }
+        }
+        // All remaining entries in map can be deleted...
+        while (!localIdMap.isEmpty()) {
+            Log.d(TAG, "Deleting items not on server anymore...");
+            int serverId = localIdMap.keySet().iterator().next();
+            List<Integer> localIdList = localIdMap.remove(serverId);
+            client.item().delete(serverId);
+            for (int localId : localIdList) {
+                provider.delete(ContentUris.withAppendedId(
+                        GaranboItemsProvider.CONTENT_URI_ITEMS, localId), null,
+                        null);
+                syncResult.stats.numDeletes++;
+                Log.d(TAG, "Deleted: " + localId);
+            }
+        }
+    }
+
+    /**
+     * Retrieve an item from the local database.
+     * 
+     * @param provider
+     *            Content provider to query.
+     * @param id
+     *            Local ID of item.
+     * @return Item with specified ID or <code>null</code> if not found.
+     * @throws RemoteException
+     *             If communication with database fails.
+     * @throws ClientException
+     *             If the item can not be parsed.
+     */
+    private Item getLocalItem(ContentProviderClient provider, Integer id)
+            throws RemoteException, ClientException {
+        Cursor cursor = provider.query(ContentUris.withAppendedId(
+                GaranboItemsProvider.CONTENT_URI_ITEMS, id), null, null, null,
+                null);
+        if (cursor.moveToFirst()) {
+            Item result = Item.fromCursor(cursor);
+            cursor.close();
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Return a map that maps server IDs to local item IDs. The value list
+     * should only contain one item for each server ID, unless the key is
+     * {@link Item#UNKNOWN_ID}.
+     * 
+     * @param provider
+     *            Content provider to query.
+     * @return Map to get local items from server ID.
+     * @throws RemoteException
+     *             If communication with local database fails.
+     */
+    private Map<Integer, List<Integer>> getLocalIdMap(
+            ContentProviderClient provider) throws RemoteException {
+        Map<Integer, List<Integer>> idMap = new HashMap<Integer, List<Integer>>();
+        Cursor cursor = provider.query(GaranboItemsProvider.CONTENT_URI_ITEMS,
+                new String[] { GaranbotDBMetaData._ID,
+                        GaranbotDBMetaData.SERVER_ID }, null, null, null);
+        while (cursor.moveToNext()) {
+            int localId = cursor.getInt(0);
+            int serverId = cursor.getInt(1);
+            if (!idMap.containsKey(serverId)) {
+                idMap.put(serverId, new ArrayList<Integer>());
+            }
+            idMap.get(serverId).add(localId);
+        }
+        cursor.close();
+        return idMap;
     }
 
     /**
