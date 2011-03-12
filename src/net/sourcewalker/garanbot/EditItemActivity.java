cfd310a2f1894cbb35e991dca51c2018fac37f40
Robert <xperimental@solidproject.de>
2011-03-13 00:47:47 +0100

    [GBOT-3] New UI for EditItemActivity.

--- src/net/sourcewalker/garanbot/EditItemActivity.java
+++ src/net/sourcewalker/garanbot/EditItemActivity.java
@@ -1,14 +1,23 @@
 package net.sourcewalker.garanbot;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import net.sourcewalker.garanbot.api.ClientException;
 import net.sourcewalker.garanbot.api.Item;
 import net.sourcewalker.garanbot.data.GaranboItemsProvider;
+import net.sourcewalker.garanbot.data.GaranbotDBMetaData;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -25,64 +34,25 @@ import android.widget.EditText;
  */
 public class EditItemActivity extends Activity {
 
-    private Button applyEditButton;
-    private Button cancelEditButton;
-    private Button mPickDate;
-    private int mYear;
-    private int mMonth;
-    private int mDay;
+    private static final String TAG = "EditItemActivity";
+    private static final int DATE_DIALOG_ID = 0;
+    private static final DateFormat DATE_FORMAT = DateFormat
+            .getDateInstance(DateFormat.SHORT);
 
-    static final int DATE_DIALOG_ID = 0;
-
-    /**
-     * OnClickListener for the apply edit button. It retrieves the data from the
-     * editable fields and stores them to the database.
-     */
-    private final OnClickListener applyEditListener = new OnClickListener() {
-
-        @Override
-        public void onClick(View arg0) {
-            String name = ((EditText) findViewById(R.id.item_name)).getText()
-                    .toString();
-            String manufacturer = ((EditText) findViewById(R.id.item_manufacturer))
-                    .getText().toString();
-            String itemtype = ((EditText) findViewById(R.id.item_itemtype))
-                    .getText().toString();
-            String location = ((EditText) findViewById(R.id.item_location))
-                    .getText().toString();
-            String notes = ((EditText) findViewById(R.id.item_notes)).getText()
-                    .toString();
-            String vendor = ((EditText) findViewById(R.id.item_vendor))
-                    .getText().toString();
-
-            Item item = new Item(Item.UNKNOWN_ID);
-            item.setName(name);
-            item.setManufacturer(manufacturer);
-            item.setItemType(itemtype);
-            item.setLocation(location);
-            item.setNotes(notes);
-            item.setPurchaseDate(new Date(mYear - 1900, mMonth, mDay));
-            item.setVendor(vendor);
-            item.setEndOfWarranty(new Date(mYear - 1898, mMonth, mDay));
-            item.setLastModified(new Date());
-
-            getContentResolver().insert(GaranboItemsProvider.CONTENT_URI_ITEMS,
-                    item.toContentValues());
-            finish();
-        }
-    };
+    private EditText nameField;
+    private EditText manufacturerField;
+    private EditText modelField;
+    private EditText locationField;
+    private EditText notesField;
+    private EditText vendorField;
+    private Button purchaseDateField;
+    private Button endWarrentyDateField;
+    private Item item;
 
     /**
-     * OnClickListener to cancel the item edit/creation.
+     * If true, item already exists in database and is only edited.
      */
-    private final OnClickListener cancelEditListener = new OnClickListener() {
-
-        @Override
-        public void onClick(View arg0) {
-            // Just close activity.
-            finish();
-        }
-    };
+    private boolean editItem = false;
 
     /**
      * OnClickListener to launch a date picker if the purchase date button is
@@ -107,9 +77,13 @@ public class EditItemActivity extends Activity {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear,
                 int dayOfMonth) {
-            mYear = year;
-            mMonth = monthOfYear;
-            mDay = dayOfMonth;
+            removeDialog(DATE_DIALOG_ID);
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(item.getPurchaseDate());
+            cal.set(Calendar.YEAR, year);
+            cal.set(Calendar.MONTH, monthOfYear);
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            item.setPurchaseDate(cal.getTime());
             updateDisplay();
         }
     };
@@ -120,37 +94,61 @@ public class EditItemActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_item);
+        setContentView(R.layout.item_edit);
 
         // capture our View elements
-        applyEditButton = (Button) findViewById(R.id.apply_button);
-        cancelEditButton = (Button) findViewById(R.id.cancel_button);
-        mPickDate = (Button) findViewById(R.id.item_purchasedate);
+        nameField = (EditText) findViewById(R.id.item_edit_name);
+        manufacturerField = (EditText) findViewById(R.id.item_edit_manufacturer);
+        modelField = (EditText) findViewById(R.id.item_edit_model);
+        locationField = (EditText) findViewById(R.id.item_edit_location);
+        notesField = (EditText) findViewById(R.id.item_edit_notes);
+        vendorField = (EditText) findViewById(R.id.item_edit_vendor);
+        purchaseDateField = (Button) findViewById(R.id.item_edit_purchase);
+        endWarrentyDateField = (Button) findViewById(R.id.item_edit_endwarranty);
 
         // add a click listeners
-        applyEditButton.setOnClickListener(applyEditListener);
-        cancelEditButton.setOnClickListener(cancelEditListener);
-        mPickDate.setOnClickListener(datePickListener);
+        purchaseDateField.setOnClickListener(datePickListener);
 
-        // get the current date
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
+        item = new Item(Item.UNKNOWN_ID);
+        final String action = getIntent().getAction();
+        if (action != null && action.length() > 0) {
+            final Uri itemUri = Uri.parse(action);
+            loadItemData(itemUri);
+        }
 
         // display the current date
         updateDisplay();
+    }
 
+    /**
+     * @param itemUri
+     */
+    private void loadItemData(Uri itemUri) {
+        Cursor cursor = managedQuery(itemUri,
+                GaranbotDBMetaData.DEFAULT_PROJECTION, null, null, null);
+        if (cursor.moveToFirst()) {
+            try {
+                item = Item.fromCursor(cursor);
+                editItem = true;
+            } catch (ClientException e) {
+                Log.e(TAG, "Can't parse item: " + e.toString());
+            }
+        }
     }
 
     /**
      * Updates the date on the date picker button.
      */
     private void updateDisplay() {
-        mPickDate.setText(new StringBuilder()
-                // Month is 0 based so add 1
-                .append(mMonth + 1).append("-").append(mDay).append("-")
-                .append(mYear).append(" "));
+        nameField.setText(item.getName());
+        manufacturerField.setText(item.getManufacturer());
+        modelField.setText(item.getItemType());
+        locationField.setText(item.getLocation());
+        notesField.setText(item.getNotes());
+        vendorField.setText(item.getVendor());
+        purchaseDateField.setText(DATE_FORMAT.format(item.getPurchaseDate()));
+        endWarrentyDateField
+                .setText(DATE_FORMAT.format(item.getEndOfWarranty()));
     }
 
     /**
@@ -160,10 +158,74 @@ public class EditItemActivity extends Activity {
     protected Dialog onCreateDialog(int id) {
         switch (id) {
         case DATE_DIALOG_ID:
-            return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
-                    mDay);
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(item.getPurchaseDate());
+            return new DatePickerDialog(this, mDateSetListener,
+                    cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH));
         }
         return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_options, menu);
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.edit_save:
+            saveItem();
+            break;
+        case R.id.edit_cancel:
+            finish();
+            break;
+        default:
+            throw new IllegalArgumentException("Unknown menu item: " + item);
+        }
+        return true;
+    }
+
+    /**
+     * Creates a new item in the database.
+     */
+    private void saveItem() {
+        String name = nameField.getText().toString();
+        String manufacturer = manufacturerField.getText().toString();
+        String itemtype = modelField.getText().toString();
+        String location = locationField.getText().toString();
+        String notes = notesField.getText().toString();
+        String vendor = vendorField.getText().toString();
+
+        item.setName(name);
+        item.setManufacturer(manufacturer);
+        item.setItemType(itemtype);
+        item.setLocation(location);
+        item.setNotes(notes);
+        item.setVendor(vendor);
+        item.setLastModified(new Date());
+
+        if (editItem) {
+            getContentResolver().update(
+                    ContentUris.withAppendedId(
+                            GaranboItemsProvider.CONTENT_URI_ITEMS,
+                            item.getLocalId()), item.toContentValues(), null,
+                    null);
+        } else {
+            getContentResolver().insert(GaranboItemsProvider.CONTENT_URI_ITEMS,
+                    item.toContentValues());
+        }
+        finish();
     }
 
 }
