5f631e32a517175d2ba35589b55a4e20a466a16e
Robert <xperimental@solidproject.de>
2011-03-20 00:13:54 +0100

    [GBOT-12] Implemented search for product via barcode.

--- src/net/sourcewalker/garanbot/EditItemActivity.java
+++ src/net/sourcewalker/garanbot/EditItemActivity.java
@@ -4,18 +4,28 @@ import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import net.sourcewalker.garanbot.api.BarcodeResult;
 import net.sourcewalker.garanbot.api.ClientException;
+import net.sourcewalker.garanbot.api.GaranboClient;
 import net.sourcewalker.garanbot.api.Item;
 import net.sourcewalker.garanbot.data.GaranboItemsProvider;
 import net.sourcewalker.garanbot.data.GaranbotDBMetaData;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.ContentUris;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnDismissListener;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -25,6 +35,10 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * This activity should enable the user to create and edit items in the local
@@ -39,8 +53,13 @@ public class EditItemActivity extends Activity {
     private static final String TAG = "EditItemActivity";
     private static final int DIALOG_PURCHASE_DATE = 10;
     private static final int DIALOG_WARRANTY_DATE = 11;
+    private static final int DIALOG_PROGRESS_BARCODE = 12;
+    private static final int DIALOG_CONFIRM_BARCODE = 13;
+    private static final int DIALOG_RESULT_BARCODE = 14;
     private static final DateFormat DATE_FORMAT = DateFormat
             .getDateInstance(DateFormat.SHORT);
+    private static final String EXTRA_BARCODE = "barcode";
+    private static final String EXTRA_RESULT = "result";
 
     private EditText nameField;
     private EditText manufacturerField;
@@ -51,6 +70,7 @@ public class EditItemActivity extends Activity {
     private Button purchaseDateField;
     private Button endWarrentyDateField;
     private Item item;
+    private BarcodeSearchTask barcodeTask;
 
     /**
      * If true, item already exists in database and is only edited.
@@ -130,6 +150,8 @@ public class EditItemActivity extends Activity {
             loadItemData(itemUri);
         }
 
+        barcodeTask = new BarcodeSearchTask();
+
         // display the current date
         updateDisplay();
     }
@@ -179,32 +201,103 @@ public class EditItemActivity extends Activity {
      */
     @Override
     protected Dialog onCreateDialog(final int id, final Bundle bundle) {
-        DatePickerDialog dialog;
-        final Calendar cal = Calendar.getInstance();
+        Dialog dialog;
         switch (id) {
         case DIALOG_PURCHASE_DATE:
-            cal.setTime(item.getPurchaseDate());
-            dialog = new DatePickerDialog(this, setPurchaseDateListener,
-                    cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH));
+            dialog = createDateDialog(id, item.getPurchaseDate(),
+                    setPurchaseDateListener);
             break;
         case DIALOG_WARRANTY_DATE:
-            cal.setTime(item.getEndOfWarranty());
-            dialog = new DatePickerDialog(this, setWarrantyDateListener,
-                    cal.get(Calendar.YEAR), cal.get(Calendar.MONDAY),
-                    cal.get(Calendar.DAY_OF_MONTH));
+            dialog = createDateDialog(id, item.getEndOfWarranty(),
+                    setWarrantyDateListener);
+            break;
+        case DIALOG_PROGRESS_BARCODE:
+            dialog = createProgressDialog();
+            break;
+        case DIALOG_CONFIRM_BARCODE:
+            dialog = new AlertDialog.Builder(this)
+                    .setTitle(R.string.edit_barcode_dialog_title)
+                    .setMessage(R.string.edit_barcode_dialog_confirm)
+                    .setCancelable(false)
+                    .setPositiveButton(android.R.string.yes,
+                            new BarcodeConfirmListener(bundle))
+                    .setNegativeButton(android.R.string.no, null).create();
+            break;
+        case DIALOG_RESULT_BARCODE:
+            dialog = new AlertDialog.Builder(this)
+                    .setTitle(R.string.edit_barcode_dialog_title)
+                    .setMessage(R.string.edit_barcode_dialog_result)
+                    .setCancelable(false)
+                    .setPositiveButton(android.R.string.yes,
+                            new BarcodeApplyListener(bundle))
+                    .setNegativeButton(android.R.string.no, null).create();
             break;
         default:
             throw new IllegalArgumentException("Unknown dialog: " + id);
         }
+        return dialog;
+    }
+
+    private ProgressDialog createProgressDialog() {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle(getString(R.string.edit_barcode_dialog_title));
+        dialog.setMessage(getString(R.string.edit_barcode_dialog_progress));
+        dialog.setCancelable(true);
+        dialog.setOnCancelListener(barcodeTask);
+        return dialog;
+    }
+
+    private DatePickerDialog createDateDialog(final int dialogId,
+            final Date date, final OnDateSetListener listener) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        final DatePickerDialog dialog = new DatePickerDialog(this, listener,
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONDAY),
+                cal.get(Calendar.DAY_OF_MONTH));
         dialog.setOnDismissListener(new OnDismissListener() {
 
             @Override
             public void onDismiss(final DialogInterface arg0) {
-                removeDialog(id);
+                removeDialog(dialogId);
             }
         });
         return dialog;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onPrepareDialog(int, android.app.Dialog)
+     */
+    @Override
+    protected void onPrepareDialog(final int id, final Dialog dialog,
+            final Bundle extras) {
+        final String messageFormat, barcode;
+        switch (id) {
+        case DIALOG_CONFIRM_BARCODE:
+            final AlertDialog confirmDialog = (AlertDialog) dialog;
+            messageFormat = getString(R.string.edit_barcode_dialog_confirm);
+            barcode = extras.getString(EXTRA_BARCODE);
+            confirmDialog.setMessage(String.format(messageFormat, barcode));
+            break;
+        case DIALOG_PROGRESS_BARCODE:
+            final ProgressDialog progressDialog = (ProgressDialog) dialog;
+            messageFormat = getString(R.string.edit_barcode_dialog_progress);
+            barcode = extras.getString(EXTRA_BARCODE);
+            progressDialog.setMessage(String.format(messageFormat, barcode));
+            break;
+        case DIALOG_RESULT_BARCODE:
+            final AlertDialog applyDialog = (AlertDialog) dialog;
+            messageFormat = getString(R.string.edit_barcode_dialog_result);
+            final BarcodeResult result = extras.getParcelable(EXTRA_RESULT);
+            applyDialog.setMessage(String.format(messageFormat,
+                    result.getName()));
+            break;
+        case DIALOG_PURCHASE_DATE:
+        case DIALOG_WARRANTY_DATE:
+            break;
+        default:
+            throw new IllegalArgumentException("Unknown dialog id: " + id);
+        }
     }
 
     /*
@@ -230,6 +323,14 @@ public class EditItemActivity extends Activity {
         case R.id.edit_cancel:
             setResult(RESULT_CANCELED);
             finish();
+            break;
+        case R.id.edit_scan:
+            IntentIntegrator.initiateScan(this,
+                    getString(R.string.scan_install_title),
+                    getString(R.string.scan_install_message),
+                    getString(android.R.string.yes),
+                    getString(android.R.string.no),
+                    IntentIntegrator.PRODUCT_CODE_TYPES);
             break;
         default:
             throw new IllegalArgumentException("Unknown menu item: " + item);
@@ -263,6 +364,40 @@ public class EditItemActivity extends Activity {
         finish();
     }
 
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onActivityResult(int, int,
+     * android.content.Intent)
+     */
+    @Override
+    protected void onActivityResult(final int requestCode,
+            final int resultCode, final Intent data) {
+        final IntentResult scanResult = IntentIntegrator.parseActivityResult(
+                requestCode, resultCode, data);
+        if (scanResult != null) {
+            final String contents = scanResult.getContents();
+            final String formatName = scanResult.getFormatName();
+            if (contents == null || formatName == null) {
+                Toast.makeText(this, R.string.scan_cancelled, Toast.LENGTH_LONG);
+            } else {
+                startParseBarcode(contents);
+            }
+        }
+    }
+
+    /**
+     * Show a progress dialog while the barcode is sent to the Garanbo server to
+     * search for product details.
+     * 
+     * @param contents
+     *            Barcode as String
+     */
+    private void startParseBarcode(final String contents) {
+        final Bundle extras = new Bundle();
+        extras.putString(EXTRA_BARCODE, contents);
+        showDialog(DIALOG_CONFIRM_BARCODE, extras);
+    }
+
     private class DateDialogButtonListener implements OnClickListener {
 
         private final int dialogId;
@@ -274,6 +409,111 @@ public class EditItemActivity extends Activity {
         @Override
         public void onClick(final View v) {
             showDialog(dialogId);
+        }
+
+    }
+
+    public class BarcodeConfirmListener implements
+            android.content.DialogInterface.OnClickListener {
+
+        private final Bundle extras;
+
+        public BarcodeConfirmListener(final Bundle extras) {
+            this.extras = extras;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see
+         * android.content.DialogInterface.OnClickListener#onClick(android.content
+         * .DialogInterface, int)
+         */
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+            showDialog(DIALOG_PROGRESS_BARCODE, extras);
+
+            final String barcode = extras.getString(EXTRA_BARCODE);
+            barcodeTask.execute(barcode);
+        }
+
+    }
+
+    private class BarcodeApplyListener implements
+            android.content.DialogInterface.OnClickListener {
+
+        private final Bundle extras;
+
+        public BarcodeApplyListener(final Bundle extras) {
+            this.extras = extras;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see
+         * android.content.DialogInterface.OnClickListener#onClick(android.content
+         * .DialogInterface, int)
+         */
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+            final BarcodeResult result = extras.getParcelable(EXTRA_RESULT);
+            item.setName(result.getName());
+            item.setManufacturer(result.getManufacturer());
+            item.setItemType(result.getType());
+
+            updateDisplay();
+        }
+    }
+
+    private class BarcodeSearchTask extends
+            AsyncTask<String, Void, BarcodeResult> implements OnCancelListener {
+
+        /*
+         * (non-Javadoc)
+         * @see android.os.AsyncTask#doInBackground(Params[])
+         */
+        @Override
+        protected BarcodeResult doInBackground(final String... params) {
+            final String barcode = params[0];
+            final AccountManager accountManager = AccountManager
+                    .get(EditItemActivity.this);
+            final Account[] accounts = accountManager
+                    .getAccountsByType(getString(R.string.account_type));
+            if (accounts.length == 1) {
+                final Account account = accounts[0];
+                final String password = accountManager.getPassword(account);
+                final GaranboClient client = new GaranboClient(account.name,
+                        password);
+                try {
+                    return client.searchEan(barcode);
+                } catch (ClientException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+         */
+        @Override
+        protected void onPostExecute(BarcodeResult result) {
+            dismissDialog(DIALOG_PROGRESS_BARCODE);
+            Bundle extras = new Bundle();
+            extras.putParcelable(EXTRA_RESULT, result);
+            showDialog(DIALOG_RESULT_BARCODE, extras);
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see
+         * android.content.DialogInterface.OnCancelListener#onCancel(android
+         * .content.DialogInterface)
+         */
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            this.cancel(true);
         }
 
     }
