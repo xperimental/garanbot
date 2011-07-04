dd80fbb9d811642071c00e42872a882ded834f42
Robert <xperimental@solidproject.de>
2011-07-04 23:14:53 +0200

    Added Api-Key.

---
+++ src/net/sourcewalker/garanbot/api/ApiConstants.java
@@ -0,0 +1,9 @@
+package net.sourcewalker.garanbot.api;
+
+final class ApiConstants {
+
+    public static final String BASE = "https://www.garanbo.de/rest/v1";
+
+    public static final String KEY = "";
+
+}
